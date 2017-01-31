5aa10cc8a30fdcf061fa18f990fdaf859bf9f1f9
Pierre Guillemot <pierre.guillemot@icflix.com>
2017-01-31 14:44:35 +0800

    Restrict admin actions on users

--- cms/users/admin.py
+++ cms/users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import FrontUser
 
-admin.site.register(FrontUser)
+class FrontUserAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'email', 'password',)
+
+    def has_add_permission(self, request):
+        return False
+
+admin.site.register(FrontUser, FrontUserAdmin)
