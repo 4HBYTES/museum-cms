90c75be4e15255a4d759ba913d928080a493fe10
Pierre Guillemot <pierre.guillemot@icflix.com>
2017-02-01 17:35:12 +0800

    Refactor to class based views for the products module

--- cms/products/urls.py
+++ cms/products/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.get_all, name='get_all'),
-    url(r'^(?P<product_id>[0-9a-z\-]+)$', views.get_one, name='get_one'),
+    url(r'^$', views.ProductsView.as_view(), name='get_all'),
+    url(r'^(?P<product_id>[0-9a-z\-]+)$', views.ProductView.as_view(), name='get_one'),
 ]
--- cms/products/views.py
+++ cms/products/views.py
@@ -1,29 +1,34 @@
 from django.http import HttpResponse
+from django.views.generic import View
 from .models import Product
 import json
 
 
-def get_all(request):
-    all_products = []
-    products = Product.objects.all()
-    for product in products:
-        all_products.append(product.to_view())
+class ProductsView(View):
 
-    json_products = json.dumps(all_products)
-    return HttpResponse(json_products, content_type='application/json')
+    def get(self, request):
+        all_products = []
+        products = Product.objects.all()
+        for product in products:
+            all_products.append(product.to_view())
+
+        json_products = json.dumps(all_products)
+        return HttpResponse(json_products, content_type='application/json')
 
 
-def get_one(request, product_id):
-    product = None
-    try:
-        product = Product.objects.get(id=product_id)
-    except Product.DoesNotExist:
-        json_error = json.dumps({'error': 'does not exists'})
-        return HttpResponse(
-            json_error,
-            status=404,
-            content_type='application/json'
-        )
+class ProductView(View):
 
-    json_product = json.dumps(product.to_view())
-    return HttpResponse(json_product, content_type='application/json')
+    def get(self, request, product_id):
+        product = None
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            json_error = json.dumps({'error': 'does not exists'})
+            return HttpResponse(
+                json_error,
+                status=404,
+                content_type='application/json'
+            )
+
+        json_product = json.dumps(product.to_view())
+        return HttpResponse(json_product, content_type='application/json')
